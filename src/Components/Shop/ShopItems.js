80c2bdbf9ee86c22b710955d290f0094410cdd62
Shubham <25881429+shoebham@users.noreply.github.com>
2022-10-12 11:06:06 +0530

    add items

--- src/Components/Shop/ShopItems.js
+++ src/Components/Shop/ShopItems.js
@@ -20,8 +20,15 @@ const ShopItems = () => {
         <div className="shop-items">
             {items.map(item => (
                 <div key={item.id} className="shop-item">
-                    <div className="shop-item-image"><img src={item.images[2]}></img></div>
+                    <img class="shop-item-image" src={item.images[2]}/>
                     <div className="shop-item-name">{item.title}</div>
+                    <div className="shop-item-price">{item.price}</div>
+                    <div className="shop-item-buttons"> 
+                        <div className="shop-item-decrease"><button>-</button></div>
+                        <div className="shop-item-quantity">1</div>
+                        <div className="shop-item-increase"><button>+</button></div>
+                    </div>
+                    <div className="shop-item-button">Add to cart</div>
                 </div>
             ))}
         </div>
