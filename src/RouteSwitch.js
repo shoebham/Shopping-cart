cd58882291bf2e763452402bc77623b14fb9af0d
Shubham <25881429+shoebham@users.noreply.github.com>
2022-10-31 16:25:08 +0530

    fix again

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,6 +1,6 @@
 
 import { useState } from "react";
-import {Routes,Route,HashRouter } from "react-router-dom";
+import {Routes,Route,BrowserRouter} from "react-router-dom";
 import Cart from "./Components/Cart";
 import Home from "./Components/Home";
 import Nav from "./Components/Nav";
@@ -9,14 +9,14 @@ import Shop from "./Components/Shop/Shop";
 const RouteSwitch = () => {
     const [cart,setCart] = useState({});
     return (
-        <HashRouter>
+        <BrowserRouter basename="shopping-cart">
         <Nav cart={cart}/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/shop" element={<Shop cart={cart} setCart={setCart}/>}/>
             </Routes>
         <Cart cart={cart} setCart={setCart}/>
-        </HashRouter >
+        </BrowserRouter>
     )
 }
 export default RouteSwitch;
