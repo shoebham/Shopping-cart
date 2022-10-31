00fbc4fbe53b7da3a9b044ede39862d5fc91db2e
Shubham <25881429+shoebham@users.noreply.github.com>
2022-10-31 15:53:01 +0530

    change cart style

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -32,48 +32,63 @@ const Cart = (props)=>{
     }
     return(
         (
-        Object.keys(cart).length>0&&
-        <div className="cart">
-            <h2>Cart 
-            <button className="close-cart slide-right" onClick={()=>{
-                document.querySelector(".cart").classList.toggle("show");
-            }}>X</button>
-            </h2>
-            {Object.keys(cart).map(item => (
-                <div key={item} className="cart-item">
-                    <img className="cart-item-image" src={cart[item].images[2]} alt="you can't afford this"/>
-                    <div className="text-items">
-                        <div className="cart-item-name">{cart[item].title}</div>
-                        <div className="cart-item-price">${cart[item].price}</div>
-                        <div className="cart-item-quantity">
-                        <input type="number" min="1" className="quantity cart-quantity" id={"cart-quant-"+item} 
-                        value={cart[item].quantity} onChange={()=>{
-                            if(document.getElementById(`cart-quant-${item}`).value>0){
-                            setCart((prevCart)=>({...prevCart,[item]:{...cart[item],quantity:document.getElementById(`cart-quant-${item}`).value}}));
-                            }
-                        }}>
-                        </input>
+            Object.keys(cart).length===0?
+            <div className="cart">
+                 <button className="close-cart slide-right" onClick={()=>{
+                    document.querySelector(".cart").classList.toggle("show");
+                }}>X</button>
+                <div className="cart-header">
+                    <h1>Cart</h1>
+                    
+                </div>
+                <div className="cart-items">
+                    <div className="cart-item">
+                        <div className="cart-item-name">Your cart is empty</div>
                     </div>
-                        <div className="subtotal">
-                            <span className="subtotal-price">${cart[item].price*cart[item].quantity}</span>
+                </div>
+            </div>
+            :
+            <div className="cart">
+                <h2>Cart 
+                <button className="close-cart slide-right" onClick={()=>{
+                    document.querySelector(".cart").classList.toggle("show");
+                }}>X</button>
+                </h2>
+                {Object.keys(cart).map(item => (
+                    <div key={item} className="cart-item">
+                        <img className="cart-item-image" src={cart[item].images[2]} alt="you can't afford this"/>
+                        <div className="text-items">
+                            <div className="cart-item-name">{cart[item].title}</div>
+                            <div className="cart-item-price">${cart[item].price}</div>
+                            <div className="cart-item-quantity">
+                            <input type="number" min="1" className="quantity cart-quantity" id={"cart-quant-"+item} 
+                            value={cart[item].quantity} onChange={()=>{
+                                if(document.getElementById(`cart-quant-${item}`).value>0){
+                                setCart((prevCart)=>({...prevCart,[item]:{...cart[item],quantity:document.getElementById(`cart-quant-${item}`).value}}));
+                                }
+                            }}>
+                            </input>
                         </div>
-                        <button className="delete-button slide-right" onClick={()=>{deleteFromCart(item)}}> Del </button>
+                            <div className="subtotal">
+                                <span className="subtotal-price">${cart[item].price*cart[item].quantity}</span>
+                            </div>
+                            <button className="delete-button slide-right" onClick={()=>{deleteFromCart(item)}}> Del </button>
+                        </div>
                     </div>
+                ))}
+                {/* {cart.map(item => (
+                    <div key={item.id} className="cart-item">
+                        <img className="cart-item-image" src={item.images[2]} alt="you can't afford this"/>
+                        <div className="cart-item-name">{item.title}</div>
+                        <div className="cart-item-price">{item.price}</div>
+                    </div>
+                ))} */}
+                <div className="total">
+                    <h3>Total:- <span className="total-text">${total}</span></h3>
                 </div>
-            ))}
-            {/* {cart.map(item => (
-                <div key={item.id} className="cart-item">
-                    <img className="cart-item-image" src={item.images[2]} alt="you can't afford this"/>
-                    <div className="cart-item-name">{item.title}</div>
-                    <div className="cart-item-price">{item.price}</div>
+                <div className="cart-button">
                 </div>
-            ))} */}
-            <div className="total">
-                <h3>Total:- <span className="total-text">${total}</span></h3>
             </div>
-            <div className="cart-button">
-            </div>
-        </div>
         )
     )
 }
